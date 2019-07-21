fc473ae5ca08644801522c50ff49ef88512efeb2
사막여우 <49156892+Doyosae@users.noreply.github.com>
2019-07-22 05:46:20 +0900

    Update 84% NIN Convolution.py

--- Network In Network/84% NIN Convolution.py
+++ Network In Network/84% NIN Convolution.py
@@ -15,6 +15,7 @@ print ("검사 이미지의 크기           ", np.shape (TestDataSet))
 print ("검사 라벨링의 크기           ", np.shape (TestLabel_OneHOtEncoding))
 
 
+# Session 1. 필요한 함수를 정의한다. 배치 셔플 함수, 신경망 함수
 # 입력으로 들어가는 데이터 세트의 크기만큼 np.arange를 이용하여 List를 생성 (Cifar-10 데이터는 50,000개 이므로 50,000 리스트 생성)
 # 이제 이 리스트의 원소들을 shuffle 해준다. 랜덤으로 정렬된 원소의 인덱스에 해당하는 데이터들을 뽑아서 ShuffleSet를 새로 생성
 def Build_NextBatch_Function (number, data, labels) :
@@ -85,6 +86,7 @@ def Build_NetworkNetwork_Function (inputs) :
     return Logits, Predict
 
 
+# Session 2. 필요한 상수, 변수, 함수 호출, 훈련, 손실도, 정확도 계산하는 구문을 입력
 Size1 = 256
 Size2 = 128
 Size3 = 64
@@ -98,18 +100,23 @@ LearningRate = 0.001
 
 X = tf.placeholder (tf.float32, shape = [None, 32, 32, 3])
 Y = tf.placeholder (tf.float32, shape = [None, 10])
+
+# bool 자료형은 True, False을 반환하는 1비트 자료형
+# 배치 정규화 과정에서 트레이닝 시에 배치 정규화를 하는지 안하는지 여부에 관여할 때 쓰인다.
+# bool형 자료형을 is_training의 feed_dict 인자로 전달한 후 True를 
 keep_prob = tf.placeholder(tf.float32)
 is_training = tf.placeholder(tf.bool)
 
 Logits, Predict = Build_NetworkNetwork_Function (X)
 
+# 1. 손실도를 계산하고 그것을 최소화하는 학습을 진행할 것
 Lossfunction = tf.reduce_mean (tf.nn.softmax_cross_entropy_with_logits_v2 (labels = Y, logits = Logits))
 LossTraining = tf.train.RMSPropOptimizer(LearningRate, decay = 0.9, momentum = 0.9).minimize(Lossfunction)
-
 
 # 2. CorrectPrediction에 들어가서 정확도를 예측하는데에 쓰일 것
 CorrectPrediction = tf.equal (tf.argmax(Y, 1), tf.argmax(Predict, 1))
 Accuracy = tf.reduce_mean (tf.cast (CorrectPrediction, tf.float32))
+
 
 with tf.Session () as sess :
     
@@ -118,15 +125,14 @@ with tf.Session () as sess :
     print ("텐서플로우 세션을 열어서 학습을 시작합니다.")
     print ("......................")
     
-    # 5000 Step만큼 최적화를 수행, 200 Step마다 학습 데이터에 대해여 정확도와 손실을 출력
+    # Epochs Step만큼 기계 학습을 시작
     for Epoch in range (Epochs) :
         
         print ("Epoch is... ... %d회" %Epoch)  
         PrintLossfunction = 0.000
         
         for i in range (390) :
-            
-            # IsTraining을 True로 켜고 진행
+            # IsTraining의 bool 자료형을 True 값으로 전달한다.
             trainBatch = Build_NextBatch_Function (BatchSize, TrainDataSet, TrainLabel_OneHotEncoding.eval())
             PrintLossTraining = sess.run (LossTraining, feed_dict = {X : trainBatch[0], Y : trainBatch[1], 
                                                                      keep_prob : 0.7, is_training : True})
@@ -134,21 +140,18 @@ with tf.Session () as sess :
                                                                       keep_prob : 1.0, is_training : True})
             
         PrintLossfunction = PrintLossfunction / 390
-              
         print ("손실도   %f" %PrintLossfunction)
         
-        
-        
+
         # 한 번 Epoch가 끝날 때마다 학습이 끝나면 테스트 데이터(10000개)에 대한 정확도를 출력
         TestAccuracy = 0.000
 
         for i in range (10) :
-
             testBatch = Build_NextBatch_Function (1000, TestDataSet, TestLabel_OneHOtEncoding.eval())
             TestAccuracy = TestAccuracy + sess.run (Accuracy, feed_dict = {X : testBatch[0], Y : testBatch[1], 
                                                                            keep_prob : 1.0, is_training : True})
 
-
-        # 테스트 데이터 10000개를 1000개 단위의 배치로 잘라서 각 배치마다의 정확도를 측정한 후, 모두 더한 다음 10으로 나누는 것
+        # 테스트 데이터 10,000개를 1,000개 단위의 배치로 잘라서 각 배치의 Acc를 계산한다.
+        # 10개의 Acc를 모두 더한 후, 10으로 나눈 Avg Acc를 Epoch 당 테스트 정확도로 간주한다.
         TestAccuracy = TestAccuracy / 10
         print("테스트 데이터 정확도: %f" % TestAccuracy)
